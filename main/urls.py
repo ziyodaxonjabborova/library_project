85c42ee24f03ae93a9f5c3b26f099cffd29e2c11
Jabborova Ziyodaxon <ziyodaxon1309@gmail.com>
2025-11-16 18:19:13 +0500

    CRUD was done

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home,single_todo_view
+from .views import home,single_todo_view,delete_todo,edit_todo
 
 urlpatterns=[
     path("",home),
-    path("batafsil/<int:id>/",single_todo_view)
+    path("batafsil/<int:id>/",single_todo_view),
+    path("delete/<int:id>/",delete_todo),
+    path("edit/<int:id>/",edit_todo)
+   
     
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Todo
 
+#home page
 def home(request):
+    
+    if request.method=="POST":
+        title=request.POST.get("title")
+        desc=request.POST.get("desc")
+        status=request.POST.get("status")
+        
+        Todo.objects.create(
+            title=title,
+            desc=desc,
+            status=status
+            
+        )
+    
     data={
         'todos':Todo.objects.all()
     }
     return render(request,'todo.html',context=data)
 
+#batafsil page
 def single_todo_view(request,id):
     todo=Todo.objects.get(id=id)
     return render(request,"single_todo.html",context={"todo":todo})
+
+#delete page
+def delete_todo(request,id):
+    try:
+        todo=Todo.objects.get(id=id)
+    except:
+        return render(request,"delete.html",context={"message":"Bunday topshiriq topilmadi!"})
+    
+    if request.method=="POST":
+        todo.delete()
+        return redirect("/")
+    
+    return render(request,"delete.html",context={'todo':todo})
+
+#update page
+
+def edit_todo(request, id):
+    todo = Todo.objects.get(id=id)
+
+    if request.method == "POST":
+        title = request.POST.get("title")
+        desc = request.POST.get("desc")
+        status = request.POST.get("status")
+
+        todo.title = title
+        todo.desc = desc
+        todo.status = status
+        todo.save()
+
+        return redirect("/")
+
+    return render(request, "edit.html", context={"todo": todo})
+
+        
+
+    
+
